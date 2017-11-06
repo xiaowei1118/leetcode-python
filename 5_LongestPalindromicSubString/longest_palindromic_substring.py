cd70909dbbd1b4e25d46305dd886a78f9a121f54
zhengzhihao <zhengzhihao@zhiweicloud.com>
2017-11-06 22:45:09 +0800

    :sparkles: complete longgest palindromic substring issue

--- 5_LongestPalindromicSubString/longest_palindromic_substring.py
+++ 5_LongestPalindromicSubString/longest_palindromic_substring.py
@@ -25,7 +25,7 @@ def longestPalindrome(s):
     max_length = 0
     dict = {}
     for index, item in enumerate(s):
-        if item in s[index+1:]:
+        if item in s[index + 1:]:
             m = s.rindex(item)
 
             while m > 0:
@@ -33,9 +33,9 @@ def longestPalindrome(s):
                 for i in range(index, len(s)):
                     if i >= j:
                         # 回文
-                        str = s[index:m+1]
-                        max_length = max(len(s[index:m+1]),max_length)
-                        dict[len(s[index:m+1])] = str
+                        str = s[index:m + 1]
+                        max_length = max(len(s[index:m + 1]), max_length)
+                        dict[len(s[index:m + 1])] = str
                         break
 
                     if s[i] != s[j]:
@@ -51,4 +51,39 @@ def longestPalindrome(s):
     else:
         return s[0]
 
-print longestPalindrome("aaabaaaa")
+
+# 动态规划
+# P(i, j) = ( P(i+1, j-1)  and S_i == S_j ) P(i,j)=(P(i+1,j−1) and S(​i)==S​(j)
+# p(i,i) = true and  p(i,i+1) == (S_i == S_i+1)
+
+def getPalindrome(s, start, end):
+    i = 0
+    while i < start + 1 and i < len(s) - end:
+        if s[start - i] != s[end + i]:
+            i -= 1
+            break
+
+        i += 1
+    else:
+        i -= 1
+
+    return s[start - i:end + i + 1]
+
+
+def longestPalindrome_dynamic(s):
+    max_palindrome = s[0]
+    for index, value in enumerate(s):
+        palindrome_middle = getPalindrome(s, index, index)
+        if len(max_palindrome) < len(palindrome_middle):
+            max_palindrome = palindrome_middle
+
+        if index < len(s)-1 and value == s[index + 1]:
+            palindrome = getPalindrome(s, index, index + 1)
+            if len(palindrome) > len(max_palindrome):
+                max_palindrome = palindrome
+
+    return max_palindrome
+
+
+# print longestPalindrome("aaabaaaa")
+print longestPalindrome_dynamic("aaabaaa")
