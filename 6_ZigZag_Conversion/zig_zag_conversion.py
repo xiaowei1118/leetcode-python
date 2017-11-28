49a4a3fad2de2c0c54a59651c9f3cfbda53f7536
zhengzhihao <zhengzhihao@zhiweicloud.com>
2017-11-28 15:03:11 +0800

    :sparkles: complete zigZag conversion

--- 6_ZigZag_Conversion/zig_zag_conversion.py
+++ 6_ZigZag_Conversion/zig_zag_conversion.py
@@ -1,3 +1,18 @@
+# -*- coding=utf-8 -*-
+'''
+    The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+    P   A   H   N
+    A P L S I I G
+    Y   I   R
+    And then read line by line: "PAHNAPLSIIGYIR"
+    Write the code that will take a string and make this conversion given a number of rows:
+
+    string convert(string text, int nRows);
+    convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+'''
+
+
 def convert(s, numRows):
     """
     :type s: str
@@ -7,6 +22,9 @@ def convert(s, numRows):
 
     result = ""
     for k in range(0, numRows):
+        if numRows == 1:     # 如果是单行，直接返回字符串，不参与计算
+            return s
+
         j = 2
         if k < len(s):
             bs = s[k]
@@ -14,22 +32,24 @@ def convert(s, numRows):
             return result
 
         while True:
-            index = j * (numRows - 1) - k
+            next_line_right = j * (numRows - 1)
+            index = next_line_right - k
 
-            if k != numRows - 1:
+            if k != numRows - 1 and k != 0:     # 收尾行没有斜线部分
                 if index < len(s):
                     bs += s[index]
                 else:
                     break
+
+            if next_line_right + k < len(s):   # 输出Z字形的中间斜线部分
+                bs += s[next_line_right + k]
             else:
-                if j * (numRows - 1) + k < len(s):
-                    bs += s[j * (numRows - 1) + k]
-                else:
-                    break
+                break
             j = j + 2
 
         result += bs
 
     return result
 
-print convert("A", 2)
+
+print convert("PAYPALISHIRING", 3)
